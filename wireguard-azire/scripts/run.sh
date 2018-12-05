53b200dabb6b3b7f8b41fed8841c56019fce873a
Thomas Nairn <thomas@thenairn.com>
2018-12-05 23:22:04 +0000

    Removed linux-headers

--- wireguard-azire/scripts/run.sh
+++ wireguard-azire/scripts/run.sh
@@ -5,7 +5,6 @@ set -e
 # Install Wireguard. This has to be done dynamically since the kernel
 # module depends on the host kernel version.
 apt update
-apt install -y linux-headers-$(uname -r)
 apt install -y wireguard
 
 exec ./configure-azire.sh
