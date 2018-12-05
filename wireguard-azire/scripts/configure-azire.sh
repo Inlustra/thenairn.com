f229d1df91f56f5bad6b87a0f2839211a7d5abc0
Thomas Nairn <thomas@thenairn.com>
2018-12-05 23:42:19 +0000

    Changed azire output

--- wireguard-azire/scripts/configure-azire.sh
+++ wireguard-azire/scripts/configure-azire.sh
@@ -6,8 +6,14 @@ wg genkey | tee privatekey | wg pubkey > publickey
 WG_PRIVATE_KEY=`cat privatekey`
 publickey=`cat publickey`
 
+echo "Calling Azire"
+echo "$publickey"
+echo "$AZIRE_USERNAME"
+echo "$AZIRE_PASSWORD"
+echo "$AZIRE_LOCATION"
+echo ""
 azireoutput=`curl -d username=$AZIRE_USERNAME --data-urlencode password=$AZIRE_PASSWORD --data-urlencode pubkey=$publickey https://api.azirevpn.com/v1/wireguard/connect/$AZIRE_LOCATION`
-
+echo "$azireoutput"
 status=`echo azireoutput | jq '.status'`
 WG_DNS=`echo azireoutput | jq '.data.DNS'`
 WG_ADDRESS=`echo azireoutput | jq '.data.Address'`
