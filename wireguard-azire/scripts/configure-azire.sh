d9bb9f2f00bb206ea6b8a3e1248d5c6229449718
Thomas Nairn <thomas@thenairn.com>
2018-12-06 00:17:38 +0000

    exported vars

--- wireguard-azire/scripts/configure-azire.sh
+++ wireguard-azire/scripts/configure-azire.sh
@@ -3,7 +3,7 @@ apt install -y jq
 
 wg genkey | tee privatekey | wg pubkey > publickey
 
-WG_PRIVATE_KEY=`cat privatekey`
+export WG_PRIVATE_KEY=`cat privatekey`
 publickey=`cat publickey`
 
 echo "Calling Azire"
@@ -16,10 +16,10 @@ echo ""
 azireoutput=`curl -s -d username=$AZIRE_USERNAME --data-urlencode password=$AZIRE_PASSWORD --data-urlencode pubkey=$publickey https://api.azirevpn.com/v1/wireguard/connect/$AZIRE_LOCATION`
 echo "$azireoutput"
 status=`jq -r '.status' <<< "$azireoutput"`
-WG_DNS=`jq -r '.data.DNS' <<< "$azireoutput"`
-WG_ADDRESS=`jq -r '.data.Address' <<< "$azireoutput"`
-WG_PUBLIC_KEY=`jq -r '.data.PublicKey' <<< "$azireoutput"`
-WG_ENDPOINT=`jq -r '.data.Endpoint' <<< "$azireoutput"`
+export WG_DNS=`jq -r '.data.DNS' <<< "$azireoutput"`
+export WG_ADDRESS=`jq -r '.data.Address' <<< "$azireoutput"`
+export WG_PUBLIC_KEY=`jq -r '.data.PublicKey' <<< "$azireoutput"`
+export WG_ENDPOINT=`jq -r '.data.Endpoint' <<< "$azireoutput"`
 
 
 
